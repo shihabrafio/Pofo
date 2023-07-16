88ef870004767c14100d2941a099ce34990540b0
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-07-17 00:59:08 +0600

    Add App files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Complete } from './components/Complete';
+
+function App() {
+  return (
+    <div className="App">
+        <Complete/>
+    </div>
+  );
+}
+
+export default App;
